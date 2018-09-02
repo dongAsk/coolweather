e7a9b78846cd9523277002d2230ee4f463748bd4
dong <dong@gmail.com>
2018-09-02 22:56:12 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -0,0 +1,10 @@
+package com.coolweather.android;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dong on 18-9-2.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
